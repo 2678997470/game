cdfbd5b4f8248aaadd14f078b795692008032406
2678997470 <63352912+2678997470@users.noreply.github.com>
2023-01-11 05:36:12 +0800

    Update chkDomain.js

--- chkDomain.js
+++ chkDomain.js
@@ -6,7 +6,7 @@ if((/4399\.air\.wd/gi).test(navigator.userAgent)){
 	//趣核不检测域名
 }else{
 	var blnOur = false;
-	var arrOurDomain = ["http://www.4399.com","http://4399.com","http://www.4399.net","http://4399.net","http://my.4399.com","http://www.3839.com","http://3839.com","http://www2.4399.net","http://4399.somp3.com",".4399.com","http://test.4399.com","http://www.4399er.com"]
+	var arrOurDomain = ["莫恋：再续天庭0.82 33页商城 GM后台 /r/n QQ群1：726765437|QQ群2：714556668|QQ群3：392349183|QQ群4：789744589|QQ群5：106158473|By：晓澜造梦QQ群：785658327"]
 	var strLocation = document.referrer;
 	if(top!=window){
 		for (var j=0;j<arrOurDomain.length;j++){
