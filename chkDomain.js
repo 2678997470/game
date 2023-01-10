60388218d11ad5f3e5076b18c7d368b4e324d0e9
2678997470 <63352912+2678997470@users.noreply.github.com>
2023-01-11 05:20:27 +0800

    Add files via upload

---
+++ chkDomain.js
@@ -0,0 +1,23 @@
+if((/4399\.air\.wd/gi).test(navigator.userAgent)){
+	//其他微端
+}else if((/4399\.zm5\.air/gi).test(navigator.userAgent)){
+	//造梦微端不检测域名
+}else if((/app\.4399\.funnycore/gi).test(navigator.userAgent)){
+	//趣核不检测域名
+}else{
+	var blnOur = false;
+	var arrOurDomain = ["http://www.4399.com","http://4399.com","http://www.4399.net","http://4399.net","http://my.4399.com","http://www.3839.com","http://3839.com","http://www2.4399.net","http://4399.somp3.com",".4399.com","http://test.4399.com","http://www.4399er.com"]
+	var strLocation = document.referrer;
+	if(top!=window){
+		for (var j=0;j<arrOurDomain.length;j++){
+			if(strLocation.indexOf(arrOurDomain[j])>=0){
+				blnOur = true;
+				break;
+			}
+		}
+	}
+
+	if(!blnOur){
+		alert("4399小游戏，玩更多的请到 www.4399.com,或者在百度搜索4399进入本站,数万个好玩的游戏等着你来玩!");
+	}
+}
